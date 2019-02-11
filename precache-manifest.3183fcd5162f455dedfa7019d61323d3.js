02a58ee5f3b9bc924cca3661bab329af9f90a97a
rskanb <rs.kanbi@gmail.com>
2019-02-11 00:50:30 -0800

    Updates

---
+++ precache-manifest.3183fcd5162f455dedfa7019d61323d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "b3d86eced11cc4bbf785",
+    "url": "/reactPortfolio/static/js/main.b3d86ece.chunk.js"
+  },
+  {
+    "revision": "d752d90a22385e5efb8e",
+    "url": "/reactPortfolio/static/js/1.d752d90a.chunk.js"
+  },
+  {
+    "revision": "b3d86eced11cc4bbf785",
+    "url": "/reactPortfolio/static/css/main.7e7568bb.chunk.css"
+  },
+  {
+    "revision": "fe594d076e84edcc5db426cc49a4e9ef",
+    "url": "/reactPortfolio/index.html"
+  }
+];
